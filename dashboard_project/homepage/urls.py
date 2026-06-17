ae33b50ff79f09c56b41a28015481393b25a9f30
Jose Lara <159408085+joselaraj@users.noreply.github.com>
2026-06-17 00:35:34 -0500

    Delete dashboard_project/homepage directory

--- dashboard_project/homepage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [
-    path('Welcome',views.homepage_view,name='homepage_view')
-]
--- dashboard_project/homepage/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-#create the homepage view, this is basically the landing page 
-def homepage_view(request):
-    return render(request,'homepage/home.html')
